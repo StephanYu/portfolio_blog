343f832b4f1545ff9c9011f51320829f70725926
StephanYu <stephan.yu@gmail.com>
2024-01-25 14:47:28 +0100

    Refactor Menu component layout and styles

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -17,42 +17,50 @@ const Menu = ({ navbarMenu }) => {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-[var(--black-a9)]" />
-        <Dialog.Content className="bg-white z-10 fixed inset-0 w-full h-full p-6 lg:px-20 lg:py-6 focus:outline-none px-4">
-         <div className="flex items-center justify-start mb-[72px]">
-            <a
-              className="cursor-pointer flex items-center justify-center w-8 h-8"
-              href="/"
-            >
-              <img src="/default-logo.svg" alt="default-logo" />
-            </a>
-          </div>
-         
-          <nav>
-            <ul className="flex flex-col dark:text-light text-dark text-lg font-bold">
-              {
-                navbarMenu.map((item, index) => (
-                  <li key={item._uid} className="flex items-center justify-start py-[10px] gap-[10px] border-b border-gray">
-                    <div className="flex flex-col justify-start items-start"><div className="text-sm font-light">{`0${index + 1}`}</div></div>
-                    
+        <Dialog.Content className="bg-white z-10 fixed inset-0 w-full p-6 lg:py-6 focus:outline-none px-4">
+          <div className="max-w-[1280px] mx-auto lg:px-0">
+            <div className="flex items-center justify-start mb-[72px]">
+              <a
+                className="cursor-pointer flex items-center justify-center w-8 h-8"
+                href="/"
+              >
+                <img src="/default-logo.svg" alt="default-logo" />
+              </a>
+            </div>
+
+            <nav>
+              <ul className="flex flex-col gap-[10px] dark:text-light text-dark text-lg font-bold">
+                {navbarMenu.map((item, index) => (
+                  <li
+                    key={item._uid}
+                    className="flex items-center justify-start lg:py-[24px] gap-[10px] border-b border-gray"
+                  >
+                    <div className="flex flex-col justify-start items-start">
+                      <div className="text-sm font-light">{`0${
+                        index + 1
+                      }`}</div>
+                    </div>
+
                     <a
                       href={`/${item.link.cached_url}`}
-                      className="text-[28px] font-normal"
+                      className="text-[28px] lg:text-[63px] font-normal lg:font-bold"
                     >
                       {item.name}
                     </a>
                   </li>
-                ))
-              }
-            </ul>
-          </nav>
+                ))}
+              </ul>
+            </nav>
+          </div>
+
           <Dialog.Close asChild>
             <button
               type="button"
-              className="font-inherit inline-flex items-center justify-center absolute top-6 right-4 lg:right-20"
+              className="font-inherit inline-flex items-center justify-center absolute top-6 right-4 lg:right-[90px]"
               aria-label="Close"
               onClick={handleClose}
             >
-              <Cross2Icon className="w-[26px] h-[26px]"/>
+              <Cross2Icon className="w-[26px] h-[26px]" />
             </button>
           </Dialog.Close>
         </Dialog.Content>
